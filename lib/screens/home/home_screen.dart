29485f24f93bb2310b421e48318714be41814f88
Eliseo_Nava_Rodriguez <eliseo.nava@cbtis128.edu.mx>
2024-03-22 00:18:47 +0000

    actualiza2

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:doctor_app_ui_tutorial/screens/home/widgets/item_category.dart';
 import 'package:doctor_app_ui_tutorial/screens/home/widgets/item_doctor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,9 +11,12 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xffccf8d6),
         leading: Padding(
           padding: const EdgeInsets.only(left: 15, right: 5),
-          child: SvgPicture.asset('assets/svg/menu.svg'),
+          //child: SvgPicture.asset('assets/svg/menu.svg'),
+          child: Image.network(
+              "https://raw.githubusercontent.com/nava128/Img_IOS/main/Unidad_II_FlutLab_Android/icon-menu-png-4.jpg"),
         ),
         scrolledUnderElevation: 0,
         actions: const [
@@ -29,35 +32,41 @@ class HomeScreen extends StatelessWidget {
         padding: const EdgeInsets.all(15),
         children: [
           const SizedBox(height: 30),
-          Text('Find Your Desired\nDoctor', style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-            fontWeight: FontWeight.bold
-          ),),
+          Text(
+            'Encuentra tu Doctor',
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge
+                ?.copyWith(fontWeight: FontWeight.bold),
+          ),
           const SizedBox(height: 15),
           Stack(
             children: [
               Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                margin: const EdgeInsets.symmetric(vertical: 3),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Theme.of(context).colorScheme.primary.withOpacity(.05)
-                ),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search for doctors',
-                    hintStyle: TextStyle(fontWeight: FontWeight.w300),
-                    border: InputBorder.none,
-                  ),
-                )
-              ),
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  margin: const EdgeInsets.symmetric(vertical: 3),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Theme.of(context)
+                          .colorScheme
+                          .primary
+                          .withOpacity(.05)),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Buscar doctor',
+                      hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                      border: InputBorder.none,
+                    ),
+                  )),
               Positioned(
-                right: 0, top: 0, bottom: 0,
+                right: 0,
+                top: 0,
+                bottom: 0,
                 child: Container(
                   width: 80,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Theme.of(context).colorScheme.primary
-                  ),
+                      borderRadius: BorderRadius.circular(50),
+                      color: Theme.of(context).colorScheme.primary),
                   child: const Icon(CupertinoIcons.search, color: Colors.white),
                 ),
               )
@@ -66,29 +75,56 @@ class HomeScreen extends StatelessWidget {
           const SizedBox(height: 15),
           ListTile(
             contentPadding: EdgeInsets.zero,
-            title: Text('Categories', style: Theme.of(context).textTheme.titleLarge,),
+            title: Text(
+              'Categorias',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
           ),
           SizedBox(
             height: 170,
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: const [
-                ItemCategory(title: 'Heart Surgeon', icon: Icons.heart_broken, color: Colors.indigo),
-                ItemCategory(title: 'Eye Specialist', icon: Icons.visibility_outlined, color: Colors.teal),
-                ItemCategory(title: 'Skin Specialist', icon: Icons.back_hand_rounded, color: Colors.brown),
+                ItemCategory(
+                    title: 'Esp. Cardiologia',
+                    icon: Icons.heart_broken,
+                    color: Colors.indigo),
+                ItemCategory(
+                    title: 'Esp. Dentista',
+                    icon: Icons.visibility_outlined,
+                    color: Colors.teal),
+                ItemCategory(
+                    title: 'Esp. Dermatologia',
+                    icon: Icons.back_hand_rounded,
+                    color: Colors.brown),
               ],
             ),
           ),
           const SizedBox(height: 15),
           ListTile(
             contentPadding: EdgeInsets.zero,
-            title: Text('Top Doctors', style: Theme.of(context).textTheme.titleLarge,),
+            title: Text(
+              'Mejores Doctores',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
           ),
           const Column(
             children: [
-              ItemDoctor(title: 'Dr. Shahid Hussain', subtitle: 'Heart Surgeon - Hadi Hospital', image: 'assets/images/doctor1.png', color: Colors.indigo),
-              ItemDoctor(title: 'Dr. Sidra Batool', subtitle: 'Heart Surgeon - Hadi Hospital', image: 'assets/images/doctor2.png', color: Colors.deepOrange),
-              ItemDoctor(title: 'Dr. Sana Ullah', subtitle: 'Heart Surgeon - Hadi Hospital', image: 'assets/images/doctor3.png', color: Colors.brown),
+              ItemDoctor(
+                  title: 'Dr. Shahid Hussain',
+                  subtitle: 'Cardiologo - IMSS',
+                  image: 'assets/images/doctor1.png',
+                  color: Colors.indigo),
+              ItemDoctor(
+                  title: 'Dr. Sidra Batool',
+                  subtitle: 'Ginecologo - Hospital General',
+                  image: 'assets/images/doctor2.png',
+                  color: Colors.deepOrange),
+              ItemDoctor(
+                  title: 'Dr. Sana Ullah',
+                  subtitle: 'Pediatra - Cruz Roja',
+                  image: 'assets/images/doctor3.png',
+                  color: Colors.brown),
             ],
           )
         ],
--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,6 +1,6 @@
 import 'package:doctor_app_ui_tutorial/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -8,6 +8,7 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff98fbe9),
       body: Column(
         children: [
           const SizedBox(height: 50),
@@ -16,30 +17,33 @@ class WelcomeScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Choose Your Desired Doctor You Want', style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                  fontWeight: FontWeight.bold,
-                )),
+                Text('Elige a tu doctor deseado que quieras',
+                    style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        )),
                 const SizedBox(height: 10),
                 const Text(
-                  'If you are looking for an expert doctor for your treatment then you are at the right place. Here you can find your desired doctor and discuss your issues with him/her.',
-                  style: TextStyle(fontWeight: FontWeight.w300, height: 1.5),
+                  'Si buscas un médico experto para tu tratamiento entonces estás en el lugar indicado. Aquí puede encontrar el médico que desee y discutir sus problemas con él.',
+                  style: TextStyle(fontWeight: FontWeight.bold, height: 1.5),
                 ),
                 const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => const HomeScreen()));
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    foregroundColor: Colors.white
-                  ),
-                  child: const Text('Get Started'),
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      foregroundColor: Colors.white),
+                  child: const Text('Empezar'),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: SvgPicture.asset('assets/svg/welcome.svg'),
+            // child: SvgPicture.asset('assets/svg/welcome.svg'),
+            child: Image.network(
+                "https://raw.githubusercontent.com/nava128/Img_IOS/main/Unidad_II_FlutLab_Android/doctores2.jpg"),
           ),
         ],
       ),
